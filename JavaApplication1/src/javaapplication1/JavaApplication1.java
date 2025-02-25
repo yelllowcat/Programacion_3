71ae24946bf56ab63acb0cb8015cfc718423f169
David <davigonzalezdev@gmail.com>
2025-02-25 12:40:27 -0700

    Reapply "diseño de primera ventana"
    This reverts commit 125a25a64ad95d85032d19ee9a0885f60e33224f.

---
+++ JavaApplication1/src/javaapplication1/JavaApplication1.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package javaapplication1;
+
+/**
+ *
+ * @author david
+ */
+public class JavaApplication1 {
+
+    public static void main(String[] args) {
+
+        Ventana ventana = new Ventana("Login");
+        
+    }
+    
+}
