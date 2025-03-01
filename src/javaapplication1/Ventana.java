a0419ee70fced96372aa0c45cc389e6ca14c21da
David <davigonzalezdev@gmail.com>
2025-02-28 23:23:43 -0700

    diseno de calculadora de interes

--- src/javaapplication1/Ventana.java
+++ src/javaapplication1/Ventana.java
@@ -7,13 +7,16 @@ package javaapplication1;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.Font;
 import java.awt.GridLayout;
-import java.awt.Image;
-import javax.swing.ImageIcon;
+import javax.swing.BorderFactory;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
+import static javax.swing.WindowConstants.EXIT_ON_CLOSE;
 
 /**
  *
@@ -31,68 +34,140 @@ public class Ventana extends JFrame{
          
 
      }
-
-        public JPanel Calculadora(){
-            JPanel mipanel = new JPanel();
-            mipanel.setLayout(new BorderLayout(20,20));
-            mipanel.setBackground(Color.GRAY);
+     public JPanel Calculadora(){
+     JPanel mipanel = new JPanel();
+            mipanel.setLayout(new BorderLayout());
+            mipanel.setBackground(Color.black);
             mipanel.setOpaque(true);
             
-            JLabel label = new JLabel();
-            label.setBackground(Color.WHITE);
-            label.setPreferredSize(new Dimension(400, 50));
-            label.setOpaque(true);
+            JPanel panel = new JPanel();
+            panel.setLayout(new FlowLayout(0,0,20));
+            panel.setBackground(Color.white);
+            panel.setOpaque(true);
+            mipanel.add(panel,BorderLayout.CENTER);
             
+            JPanel bordeBot = new JPanel();
+            bordeBot.setBackground(Color.white);
+            mipanel.add(bordeBot,BorderLayout.SOUTH);
+                    
+            
+            
+            JLabel label = new JLabel("Interes");
+            label.setFont(new Font("Serif",Font.ITALIC,26));
+            label.setForeground(Color.pink);
+            label.setBackground(Color.white);
+           // label.setPreferredSize(new Dimension(40, 50));
+            label.setOpaque(true);
             mipanel.add(label,BorderLayout.NORTH);
             
-            JPanel botones = new JPanel();
-            botones.setBackground(Color.gray);
-            botones.setLayout(new GridLayout(4,3,10,10));
+            JPanel verde = new JPanel();
+            verde.setLayout(new BorderLayout());
+            verde.setPreferredSize(new Dimension(400, 250));
+            verde.setBackground(Color.green);
+            verde.setBorder(BorderFactory.createMatteBorder(5, 15, 5, 15, Color.white));
+            panel.add(verde,BorderLayout.CENTER);
             
-            mipanel.add(botones,BorderLayout.CENTER);
+            
+            
+            JLabel calcularInteres = new JLabel("Calcular Interes");
+            verde.add(calcularInteres,BorderLayout.NORTH);
+            
+            JPanel verdeBottom = new JPanel();
+            verdeBottom.setBackground(Color.green);
+            verde.add(verdeBottom,BorderLayout.CENTER);
+            
+            verdeBottom.setLayout(new GridLayout(4, 2,10,20));
+            
+            JLabel capital = new JLabel("Capital:");
+            capital.setBorder(BorderFactory.createMatteBorder(5, 100, 5, 0, Color.green));
+            verdeBottom.add(capital);
+            
+            JTextField fieldCapital = new JTextField();
+            fieldCapital.setBackground(Color.white);
+            fieldCapital.setOpaque(true);
+            fieldCapital.setBorder(BorderFactory.createMatteBorder(5, 15, 5, 15, Color.green));
+            verdeBottom.add(fieldCapital);
+            
+             JLabel tiempo = new JLabel("Tiempo:");
+             tiempo.setBorder(BorderFactory.createMatteBorder(5, 100, 5, 0, Color.green));
+            verdeBottom.add(tiempo);
+            
+            JTextField fieldTiempo = new JTextField();
+            fieldTiempo.setBackground(Color.white);
+            fieldTiempo.setOpaque(true);
+            fieldTiempo.setBorder(BorderFactory.createMatteBorder(5, 15, 5, 15, Color.green));
+            verdeBottom.add(fieldTiempo);
+            
+            JLabel tasa = new JLabel("Tasa interes:");
+            tasa.setBorder(BorderFactory.createMatteBorder(5, 100, 5, 0, Color.green));
+            verdeBottom.add(tasa);
+            
+            JTextField fieldTasa = new JTextField();
+            fieldTasa.setBackground(Color.white);
+            fieldTasa.setOpaque(true);
+            fieldTasa.setBorder(BorderFactory.createMatteBorder(5, 15, 5, 15, Color.green));
+            verdeBottom.add(fieldTasa);
+            
+            JButton calcular = new JButton("Calcular");
+            calcular.setBackground(Color.black);
+            calcular.setForeground(Color.white);
+            calcular.setBorder(BorderFactory.createMatteBorder(5, 30, 5, 0, Color.green));
+
+            verdeBottom.add(calcular);
+            
+            JButton cancelar = new JButton("Cancelar");
+            cancelar.setBackground(Color.BLACK);
+            cancelar.setForeground(Color.white);
+            cancelar.setBorder(BorderFactory.createMatteBorder(5, 0, 5, 30, Color.green));
+            verdeBottom.add(cancelar);
+                    
+            
+            JPanel rosa = new JPanel();
+            rosa.setPreferredSize(new Dimension(400, 100));
+            rosa.setBackground(Color.pink);
+            rosa.setBorder(BorderFactory.createMatteBorder(5, 15, 5, 15, Color.white));
+            rosa.setLayout(new GridLayout(2, 2,10,20));
+            panel.add(rosa,BorderLayout.SOUTH);
+            
+             JLabel Interes = new JLabel("Interes:");
+             Interes.setBorder(BorderFactory.createMatteBorder(5, 100, 5, 0, Color.pink));
+            rosa.add(Interes);
+            
+            JTextField fieldInteres = new JTextField();
+            fieldInteres.setBackground(Color.white);
+            fieldInteres.setOpaque(true);
+            fieldInteres.setBorder(BorderFactory.createMatteBorder(5, 0, 5, 100, Color.pink));
+            rosa.add(fieldInteres);
+            
+            JLabel monto = new JLabel("Monto:");
+            monto.setBorder(BorderFactory.createMatteBorder(5, 100, 5, 0, Color.pink));
+            rosa.add(monto);
+            
+            JTextField fieldMonto = new JTextField();
+            fieldMonto.setBackground(Color.white);
+            fieldMonto.setOpaque(true);
+            fieldMonto.setBorder(BorderFactory.createMatteBorder(5, 0, 5, 100, Color.pink));
+            rosa.add(fieldMonto);
+            
             
             JPanel botones2= new JPanel();
             botones2.setBackground(Color.gray);
-            botones2.setLayout(new GridLayout(6,1,5,5));
-
+            botones2.setLayout(new GridLayout());
             
             
-            mipanel.add(botones2,BorderLayout.EAST);
             
-            JButton[] arregloBotones = new JButton[10];
-            int i=9;
-            for(JButton boton:arregloBotones){
-                
-                boton = new JButton(""+i);
-                i--;
-                botones.add(boton);
-                boton.setBackground(Color.black);
-                boton.setForeground(Color.white);
-            }
-            JButton botonDot= new JButton(".");
-            botones.add(botonDot);
-            botonDot.setBackground(Color.black);
-            botonDot.setForeground(Color.white);
             
-            JButton[] arregloBotones2 = new JButton[6];
             
-            for (int j = 0; j < arregloBotones2.length; j++) {
-                arregloBotones2[j]= new JButton();
-                botones2.add(arregloBotones2[j]);
-             arregloBotones2[j].setBackground(Color.yellow);
-            arregloBotones2[j].setForeground(Color.black);
-            }
-            arregloBotones2[0].setText("+");
-            arregloBotones2[1].setText("-");
-            arregloBotones2[2].setText("*");
-            arregloBotones2[3].setText("/");
-            arregloBotones2[4].setText("=");
-            arregloBotones2[5].setText("CE");
             
-
             
-            return mipanel;
-        }
-    
-     
+            
+            
+            
+            
+            
+            
+            
+            
+        return mipanel;
+     }
 }
